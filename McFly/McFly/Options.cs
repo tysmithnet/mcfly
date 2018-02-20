798bc20155011e63eb5319de79e2527664506c33
Ty Smith <dts5004@gmail.com>
2018-02-19 22:38:03 -0800

    Added options file for command line args

---
+++ McFly/McFly/Options.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace McFly
+{
+    [Verb("start", HelpText = "Start the server")]
+    public class InitOptions
+    {
+        public string DatabaseFile { get; set; }
+    }
+}
