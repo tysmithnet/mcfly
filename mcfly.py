53d6cf905376dee64c1e565440080611d5ac9293
Ty Smith <dts5004@gmail.com>
2018-02-19 20:14:48 -0800

    Added initial structure to module

--- mcfly.py
+++ mcfly.py
@@ -1,12 +1,31 @@
 import sqlite3
-import pykd
+
 import argparse
 import sys
+try:
+    import pykd
+except Exception:
+    print("Unable to import pykd, run pip install pykd in your configured interpreter")
 
-def init():
-    print("hello world")
+commands = {}
+init_parser = argparse.ArgumentParser("init")
+
+commands["init"] = init_parser
+
+
+def print_help():
+    for command in commands:
+        print("{0} --help".format(command))
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "init":
-        init()
+    argv = sys.argv
+
+    # if help requested
+    if [x for x in argv if x in ["-h", "--help"]]:
+
+        # see if command was provided
+        if argv[1] in commands:
+            commands[argv[1]].print_help()
+        else:
+            print_help()
