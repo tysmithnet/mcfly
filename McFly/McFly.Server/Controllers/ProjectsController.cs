32776e8f42f33ccb67c8a3d8257aee29dca21c17
Ty Smith <dts5004@gmail.com>
2018-02-20 01:46:22 -0800

    Added hack for swagger to work after VS update

--- McFly/McFly.Server/Controllers/ProjectsController.cs
+++ McFly/McFly.Server/Controllers/ProjectsController.cs
@@ -23,11 +23,11 @@ namespace McFly.Server.Controllers
             return new JsonResult(ProjectsAccess.GetDatabases());
         }
 
-        //[HttpPost]
-        //public ActionResult Post(string projectName)
-        //{
-        //    ProjectsAccess.CreateProject(projectName);
-        //    return Ok();
-        //}
+        [HttpPost]
+        public ActionResult Post(string projectName)
+        {
+            ProjectsAccess.CreateProject(projectName);
+            return Ok();
+        }
     }
 }
