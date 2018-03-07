b2c9b89322ea2ff167690693b60030ff277023e7
Ty Smith <dts5004@gmail.com>
2018-03-07 02:05:38 -0800

    Added auto generated comments

--- McFly/McFly/StartMethod.cs
+++ McFly/McFly/StartMethod.cs
@@ -1,33 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// ***********************************************************************
+// Assembly         : mcfly
+// Author           : master
+// Created          : 03-06-2018
+//
+// Last Modified By : master
+// Last Modified On : 03-07-2018
+// ***********************************************************************
+// <copyright file="StartMethod.cs" company="">
+//     Copyright ©  2018
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
 using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Primitives;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace McFly
 {
+    /// <summary>
+    ///     Class StartMethod.
+    /// </summary>
+    /// <seealso cref="McFly.IMcFlyMethod" />
     [Export(typeof(IMcFlyMethod))]
     public class StartMethod : IMcFlyMethod
     {
+        /// <summary>
+        ///     Gets or sets the settings.
+        /// </summary>
+        /// <value>The settings.</value>
         [Import(typeof(Settings))]
         private Settings Settings { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the log.
+        /// </summary>
+        /// <value>The log.</value>
         [Import(typeof(ILog))]
         private ILog Log { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the debug eng proxy.
+        /// </summary>
+        /// <value>The debug eng proxy.</value>
         [Import(typeof(IDbgEngProxy))]
         private IDbgEngProxy DbgEngProxy { get; set; }
 
+        /// <summary>
+        ///     Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
         public string Name { get; } = "start";
-        
+
+        /// <summary>
+        ///     Processes the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns>Task.</returns>
         public void Process(string[] args)
         {
             if (string.IsNullOrWhiteSpace(Settings.LauncherPath))
-            {   
+            {
                 Log.Error("Start called but no launcher path was set. Check the settings file.");
                 return;
             }
@@ -41,7 +74,7 @@ namespace McFly
             {
                 FileName = Settings.LauncherPath,
                 CreateNoWindow = false,
-                Environment = { { "ConnectionString", Settings.ConnectionString } },
+                Environment = {{"ConnectionString", Settings.ConnectionString}},
                 UseShellExecute = false
             };
             var p = System.Diagnostics.Process.Start(startInfo);
