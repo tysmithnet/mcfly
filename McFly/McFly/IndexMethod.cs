8d36d9e46590b851977fa13de4047c97c507b83e
Ty Smith <dts5004@gmail.com>
2018-03-08 22:35:52 -0800

    Broke up IndexMethod

--- McFly/McFly/IndexMethod.cs
+++ McFly/McFly/IndexMethod.cs
@@ -17,7 +17,6 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using CommandLine;
 using McFly.Core;
 
@@ -64,25 +63,96 @@ namespace McFly
         /// <returns>Task.</returns>
         public void Process(string[] args)
         {
+            // todo: handle help
             var options = new IndexOptions();
+            Position endingPosition;
             Parser.Default.ParseArguments<IndexOptions>(args).WithParsed(o => { options = o; }).WithNotParsed(errors =>
             {
                 Log.Error($"Error: Unable to parse arguments"); // todo: add errors
             });
 
-            Position endingPosition;
-            if (options.End != null)
-            {
-                endingPosition = Position.Parse(options.End);
-            }
-            else
-            {
-                var end = DbgEngProxy.Execute("!tt 100");
-                var endMatch = Regex.Match(end, "Setting position: (?<pos>[A-F0-9]+:[A-F0-9]+)");
-                endingPosition = Position.Parse(endMatch.Groups["pos"].Value);
-            }
+            endingPosition = GetEndingPosition(options);
+            SetBreakpoints(options);
+            HitBreakpoints(endingPosition);
+        }
 
-            // clear breakpoints
+        private void HitBreakpoints(Position endingPosition)
+        {
+            var endReached = false;
+            var is32Bit =
+                Regex.Match(DbgEngProxy.Execute("!peb"), @"PEB at (?<peb>[a-fA-F0-9]+)").Groups["peb"].Value.Length ==
+                8;
+            // loop through all the set break points and record relevant values
+            while (true)
+            {
+                DbgEngProxy.RunUntilBreak();
+
+                var records = GetPositions().ToArray();
+                var breakRecord = records.Single(x => x.IsThreadWithBreak);
+                if (breakRecord.Position >= endingPosition)
+                    break;
+
+                var frames = CreateFramesForUpsert(records, breakRecord, is32Bit);
+                UpsertFrames(frames);
+            }
+        }
+
+        private List<Frame> CreateFramesForUpsert(PositionsRecord[] records, PositionsRecord breakRecord, bool is32Bit)
+        {
+            var frames = (from record in records
+                where record.Position == breakRecord.Position
+                let registerSet = DbgEngProxy.GetRegisters(record.ThreadId, Register.CoreUserRegisters64)
+                let stackTrace = DbgEngProxy.Execute("k")
+                let stackFrames = GetStackFrames(stackTrace)
+                select CreateFrame(is32Bit, record, registerSet, stackFrames)).ToList();
+            return frames;
+        }
+
+        private void UpsertFrames(List<Frame> frames)
+        {
+            using (var serverClient = new ServerClient(new Uri(Settings.ServerUrl)))
+            {
+                serverClient.UpsertFrames(Settings.ProjectName, frames);
+            }
+        }
+
+        private Frame CreateFrame(bool is32Bit, PositionsRecord record, RegisterSet registerSet,
+            List<StackFrame> stackFrames)
+        {
+            var eipRegister = is32Bit ? "eip" : "rip";
+            var instructionText = DbgEngProxy.Execute($"u {eipRegister} L1");
+            var match = Regex.Match(instructionText,
+                @"(?<sp>[a-fA-F0-9`]+)\s+[a-fA-F0-9]+\s+(?<ins>\w+)\s+(?<extra>.+)?");
+
+            var frame = new Frame
+            {
+                Position = record.Position,
+                RegisterSet = registerSet,
+                ThreadId = record.ThreadId,
+                StackFrames = stackFrames,
+                OpcodeMnemonic = match.Groups["ins"].Success ? match.Groups["ins"].Value : null,
+                DisassemblyNote = match.Groups["extra"].Success ? match.Groups["extra"].Value : null
+            };
+            return frame;
+        }
+
+        private static List<StackFrame> GetStackFrames(string stackTrace)
+        {
+            var stackFrames = (from m in Regex.Matches(stackTrace,
+                        @"(?<sp>[a-fA-F0-9`]+) (?<ret>[a-fA-F0-9`]+) (?<mod>.*)!(?<fun>.*)\+(?<off>[a-fA-F0-9x]+)?")
+                    .Cast<Match>()
+                let stackPointer = Convert.ToUInt64(m.Groups["sp"].Value.Replace("`", ""), 16)
+                let returnAddress = Convert.ToUInt64(m.Groups["ret"].Value.Replace("`", ""), 16)
+                let module = m.Groups["mod"].Value
+                let functionName = m.Groups["fun"].Value
+                let offset = Convert.ToUInt32(m.Groups["off"].Value, 16)
+                select new StackFrame(stackPointer, returnAddress, module, functionName, offset)).ToList();
+            return stackFrames;
+        }
+
+        private void SetBreakpoints(IndexOptions options)
+        {
+// clear breakpoints
             DbgEngProxy.Execute("bc *"); // todo: save existing break points and restore
 
             // set head at start
@@ -108,59 +178,22 @@ namespace McFly
                     foreach (var c in match.Groups["access"].Value)
                         DbgEngProxy.Execute($"ba {c}{match.Groups["length"].Value} {match.Groups["address"].Value}");
                 }
+        }
 
-            var endReached = false;
-            var is32Bit =
-                Regex.Match(DbgEngProxy.Execute("!peb"), @"PEB at (?<peb>[a-fA-F0-9]+)").Groups["peb"].Value.Length ==
-                8;
-            // loop through all the set break points and record relevant values
-            while (true)
+        private Position GetEndingPosition(IndexOptions options)
+        {
+            Position endingPosition;
+            if (options.End != null)
             {
-                DbgEngProxy.RunUntilBreak();
-                var records = GetPositions().ToArray();
-                var breakRecord = records.Single(x => x.IsThreadWithBreak);
-                if (breakRecord.Position >= endingPosition)
-                    break;
-
-                var frames = new List<Frame>();
-                foreach (var record in records)
-                {
-                    // all threads currently at the same breakpoint position
-                    if (record.Position != breakRecord.Position) continue;
-                    var registerSet = DbgEngProxy.GetRegisters(record.ThreadId, Register.CoreUserRegisters64);
-                    var stackTrace = DbgEngProxy.Execute("k");
-
-                    var stackFrames = (from m in Regex.Matches(stackTrace,
-                                @"(?<sp>[a-fA-F0-9`]+) (?<ret>[a-fA-F0-9`]+) (?<mod>.*)!(?<fun>.*)\+(?<off>[a-fA-F0-9x]+)?")
-                            .Cast<Match>()
-                        let stackPointer = Convert.ToUInt64(m.Groups["sp"].Value.Replace("`", ""), 16)
-                        let returnAddress = Convert.ToUInt64(m.Groups["ret"].Value.Replace("`", ""), 16)
-                        let module = m.Groups["mod"].Value
-                        let functionName = m.Groups["fun"].Value
-                        let offset = Convert.ToUInt32(m.Groups["off"].Value, 16)
-                        select new StackFrame(stackPointer, returnAddress, module, functionName, offset)).ToList();
-
-                    var eipRegister = is32Bit ? "eip" : "rip";
-                    var instructionText = DbgEngProxy.Execute($"u {eipRegister} L1");
-                    var match = Regex.Match(instructionText,
-                        @"(?<sp>[a-fA-F0-9`]+)\s+[a-fA-F0-9]+\s+(?<ins>\w+)\s+(?<extra>.+)?");
-
-                    var frame = new Frame
-                    {
-                        Position = record.Position,
-                        RegisterSet = registerSet,
-                        ThreadId = record.ThreadId,
-                        StackFrames = stackFrames,
-                        OpcodeMnemonic = match.Groups["ins"].Success ? match.Groups["ins"].Value : null,
-                        DisassemblyNote = match.Groups["extra"].Success ? match.Groups["extra"].Value : null
-                    };
-                    frames.Add(frame);
-                }
-                using (var serverClient = new ServerClient(new Uri(Settings.ServerUrl)))
-                {
-                    serverClient.UpsertFrames(Settings.ProjectName, frames);
-                }
+                endingPosition = Position.Parse(options.End);
             }
+            else
+            {
+                var end = DbgEngProxy.Execute("!tt 100"); // todo: get from trace_info
+                var endMatch = Regex.Match(end, "Setting position: (?<pos>[A-F0-9]+:[A-F0-9]+)");
+                endingPosition = Position.Parse(endMatch.Groups["pos"].Value);
+            }
+            return endingPosition;
         }
 
         /// <summary>
