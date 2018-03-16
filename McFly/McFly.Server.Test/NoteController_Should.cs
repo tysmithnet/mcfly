9525cb862e3652c96a97a50560be23825606d424
Ty Smith <dts5004@gmail.com>
2018-03-16 01:13:58 -0700

    Added NoteController tests

---
+++ McFly/McFly.Server.Test/NoteController_Should.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using McFly.Core;
+using McFly.Server.Controllers;
+using Xunit;
+
+namespace McFly.Server.Test
+{
+    public class NoteController_Should
+    {
+        [Fact]
+        public void Create_Note_On_Post()
+        {
+            // arrange
+            var controller = new NoteController();
+
+            // act
+            // assert
+            controller.Post("testing", new Position(0, 0), 1, "hi world");
+        }
+    }
+}
