0976809f5a3debddac1b0a67f312718d1056f93e
Ty Smith <dts5004@gmail.com>
2018-03-28 01:06:28 -0700

    Added note method tests

---
+++ McFly/McFly.Tests/NoteMethod_Should.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace McFly.Tests
+{
+    public class NoteMethod_Should
+    {
+        [Fact]
+        public void Not_Allow_Multiple_Note_Bodies()
+        {
+            var noteMethod = new NoteMethod();
+            Action throw1 = () => noteMethod.Process(new[] {"add", "note1", "note2"});
+            Action throw2 = () => noteMethod.Process(new[] {"add", "\"this is a note\"", "\"this is also a note\""});
+            Action throw3 = () => noteMethod.Process(new[] {"add", "note1", "-a", "note2"});
+            Action throw4 = () => noteMethod.Process(new[] {"add", "note1", "-a"});
+            Action throw5 = () => noteMethod.Process(new[] {"add", "-a", "note1"});
+
+            throw1.Should().Throw<ArgumentException>();
+            throw2.Should().Throw<ArgumentException>();
+            throw3.Should().Throw<ArgumentException>();
+            throw4.Should().NotThrow<ArgumentException>();
+            throw5.Should().NotThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void Parse_Add_Options()
+        {
+            var noteMethod = new NoteMethod();
+            var a = noteMethod.ExtractAddOptions(new[] {"note1", "-a"});
+            var a2 = noteMethod.ExtractAddOptions(new[] { "-a", "note1" });
+            var n = noteMethod.ExtractAddOptions(new[] {"this is content"});
+
+            a.IsAllThreadsAtPosition.Should().BeTrue();
+            a.Text.Should().Be("note1");
+
+            a2.IsAllThreadsAtPosition.Should().BeTrue();
+            a2.Text.Should().Be("note1");
+
+            n.IsAllThreadsAtPosition.Should().BeFalse();
+            n.Text.Should().Be("this is content");
+        }
+    }
+}
--- McFly/McFly/NoteMethod.cs
+++ McFly/McFly/NoteMethod.cs
@@ -12,8 +12,11 @@
 // <summary></summary>
 // ***********************************************************************
 
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using CommandLine;
 
 namespace McFly
@@ -46,6 +49,12 @@ namespace McFly
         public HelpInfo HelpInfo { get; } = new HelpInfoBuilder()
             .SetName("note")
             .SetDescription("Take notes on the trace")
+            .AddSubcommand(new HelpInfoBuilder()
+                .SetName("add")
+                .SetDescription("Add notes")
+                .AddExample("!mf note add \"Encryption begin\"", "Adds a note to the current frame")
+                .Build())
+            .AddExample("!mf note", "Shows all notes on the current frame")
             .Build();
 
         /// <summary>
@@ -55,21 +64,63 @@ namespace McFly
         /// <returns>Task.</returns>
         public void Process(string[] args)
         {
-            Parser.Default.ParseArguments<AddNoteOptions>(args)
-                .WithParsed(options => { });
+            if (args == null)
+                throw new NullReferenceException(nameof(args));
+            if (!args.Any())
+            {
+                // list notes
+            }
+            else
+            {
+                var command = args[0];
+                switch (command)
+                {
+                    case "add":
+                        var addOptions = ExtractAddOptions(args.Skip(1));
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException($"Unknown subcommand {command}");
+                }
+            }
+        }
+
+        protected internal AddNoteOptions ExtractAddOptions(IEnumerable<string> args)
+        {
+            var options = new AddNoteOptions();
+            var arr = args.ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var ptr = arr[i];
+
+                switch (ptr)
+                {
+                    case "-a":
+                    case "--all":
+                        options.IsAllThreadsAtPosition = true;
+                        break;
+                    default:
+                        if (options.Text == null)
+                            options.Text = ptr;
+                        else
+                            throw new ArgumentException("Found more than 1 note body");
+                        break;
+                }
+            }
+            return options;
         }
     }
 
     /// <summary>
     ///     Class AddNoteOptions.
     /// </summary>
-    [Verb("add", HelpText = "Add a note to the current position")]
     internal class AddNoteOptions
     {
+        public bool IsAllThreadsAtPosition { get; set; }
         /// <summary>
         ///     Gets or sets the content.
         /// </summary>
         /// <value>The content.</value>
-        public string Content { get; set; }
+        public string Text { get; set; }
     }
 }
