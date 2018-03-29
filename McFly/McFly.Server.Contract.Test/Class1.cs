2028beaadfd36551562b881bb080c92446275ec5
Ty Smith <dts5004@gmail.com>
2018-03-28 18:27:00 -0700

    Added McFly.Server.Contract.Test project

---
+++ McFly/McFly.Server.Contract.Test/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace McFly.Server.Contract.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void Should_Pass()
+        {
+            true.Should().Be(true);
+        }
+    }
+}
