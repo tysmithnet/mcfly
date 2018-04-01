5fbf862611bf32c7e92cd92fb5194925127f2c23
Ty Smith <dts5004@gmail.com>
2018-03-31 21:49:47 -0700

    Added frame_stackframe table and first mapper

---
+++ McFly/McFly.Server.Data.SqlServer/FrameToEntityMapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using McFly.Core;
+using McFly.Server.Data.SqlServer.Entities;
+
+namespace McFly.Server.Data.SqlServer
+{
+    public static class FrameToEntityMapper
+    {
+        public static FrameEntity ToEntity(this Frame frame)
+        {
+            var entity = new FrameEntity
+            {
+                PosHi = frame.Position.High,
+                PosLo = frame.Position.Low,
+                ThreadId = frame.ThreadId,
+                Rax = frame.RegisterSet.Rax.ToLong(),
+                Rbx = frame.RegisterSet.Rbx.ToLong(),
+                Rcx = frame.RegisterSet.Rcx.ToLong(),
+                Rdx = frame.RegisterSet.Rdx.ToLong(),
+                
+                OpCode = frame.DisassemblyLine?.OpCode,
+                DisassemblyNote = frame.DisassemblyLine?.DisassemblyNote
+            };
+
+
+            return entity;
+        }
+    }
+}
---
+++ McFly/McFly.SqlServer/Tables/frame_stackframe.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[frame_stackframe]
+(
+    [thread_id] INT NOT NULL PRIMARY KEY
+)
