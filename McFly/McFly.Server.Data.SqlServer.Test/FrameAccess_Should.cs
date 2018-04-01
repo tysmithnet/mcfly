2e887ba7713d1240ef080ba83394c09f83472d7e
Ty Smith <dts5004@gmail.com>
2018-04-01 14:28:07 -0700

    Added McFly.Server.Data.SqlServer.Test project

---
+++ McFly/McFly.Server.Data.SqlServer.Test/FrameAccess_Should.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McFly.Server.Data.SqlServer.Test
+{
+    public class FrameAccess_Should
+    {
+        public void Update_An_Existing_Frame_If_One_Exists()
+        {
+            var access = new FrameAccess();
+        }
+    }
+}
--- McFly/McFly.Server.Data.SqlServer.Test/Properties/AssemblyInfo.cs
+++ McFly/McFly.Server.Data.SqlServer.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("McFly.Server.Data.SqlServer")]
+[assembly: AssemblyTitle("McFly.Server.Data.SqlServer.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("McFly.Server.Data.SqlServer")]
+[assembly: AssemblyProduct("McFly.Server.Data.SqlServer.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5faa1707-d76b-48a6-a63c-37c510eceab5")]
+[assembly: Guid("1a1071c3-5f84-4e12-a489-8d781310af46")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("McFly.Server.Data.SqlServer.Test")]
