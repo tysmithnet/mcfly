8436857c551b885a0fcd107e19b3aa2799c5fe9e
Ty Smith <dts5004@gmail.com>
2018-04-02 02:03:59 -0700

    Added concept of ISearchCriterionConverter

--- McFly/McFly.Server/Controllers/SearchController.cs
+++ McFly/McFly.Server/Controllers/SearchController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.ModelBinding;
 using McFly.Server.Contract;
+using McFly.Server.Data.Search;
 using McFly.Server.Headers;
 
 namespace McFly.Server.Controllers
@@ -17,7 +18,16 @@ namespace McFly.Server.Controllers
         public IHttpActionResult Post([FromProjectNameHeader] string projectName, [FromUri] string index,
             [FromBody] Criterion criterion)
         {
+
             return Ok();
         }
     }
+
+    internal interface ISearchCriterionConverter
+    {
+        bool CanConvert(string conversionType);
+        ICriterion Convert(string conversionType, string input);
+    }
+
+    // todo: add register converter, etc
 }
