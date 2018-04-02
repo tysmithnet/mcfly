d850fb341509fb7098701739986de285a2f5d003
Ty Smith <dts5004@gmail.com>
2018-04-01 23:22:41 -0700

    Reimplemented visitor pattern for search request

--- McFly/McFly.Server.Data/Search/ICriterionVisitor.cs
+++ McFly/McFly.Server.Data/Search/ICriterionVisitor.cs
@@ -3,5 +3,14 @@
     public interface ICriterionVisitor
     {
         object Visit(ICriterion criterion);
+        object Visit(AndCriterion andCriterion);
+        object Visit(OrCriterion orCriterion);
+        object Visit(NotCriterion notCriterion);
+        object Visit(RegisterEqualsCriterion registerEqualsCriterion);
+        object Visit(RegisterBetweenCriterion registerBetweenCriterion);
+        object Visit(RegisterInCriterion registerInCriterion);
+        object Visit(NoteCreatedAfterCriterion noteCreatedAfterCriterion);
+        object Visit(NoteCreatedBeforeCriterion noteCreatedBeforeCriterion);
+        object Visit(NoteCreatedBetweenCriterion noteCreatedBetweenCriterion);
     }
 }
