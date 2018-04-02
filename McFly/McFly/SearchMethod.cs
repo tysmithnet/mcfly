8c1dcac9f7a2167f77497bbaf6865b7a0f022da3
Ty Smith <dts5004@gmail.com>
2018-04-02 15:02:48 -0700

    Removed dead code

--- McFly/McFly/SearchMethod.cs
+++ McFly/McFly/SearchMethod.cs
@@ -11,13 +11,6 @@ namespace McFly
         public HelpInfo HelpInfo { get; } = new HelpInfoBuilder()
             .SetName("search")
             .SetDescription("Search for indexed content")
-            //.AddExample("!mf search frame -rax 1 -mod kernel32 -fun *Create*", "Find frames where rax=1 and the mask for the frame is kernel32!*Create*")
-            //.AddExample("!mf search all regex nt*", "Search all indices for content matching the regex \"nt.*\"")
-            //.AddExample("!mf search note -text \"encryption\"", "Search for all notes containing encryption")
-            .AddExample("!mf search", "List all indexed frames (up to the setting \"MaxNumSearchResults\")")
-            .AddExample("!mf search -s 0:0 -e 100:0", "List all indexed frames between position 0:0 and 100:0")
-            .AddExample("!mf search kernel32", "Searches all indices for the general term kernel32")
-            .AddExample("!mf search | where rax -eq 1 and mod -contains 32 | fields rax rip mod fun", "Get the rax, rip, mod, fun all frames where rax=1 the instruction module contains 32")
             .Build();
                                                                          
         [Import]
