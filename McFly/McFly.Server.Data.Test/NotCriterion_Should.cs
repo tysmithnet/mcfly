5381f0ac4126f542167473bc22995c7193fe9849
Ty Smith <dts5004@gmail.com>
2018-04-04 19:07:14 -0700

    Added data test project

---
+++ McFly/McFly.Server.Data.Test/NotCriterion_Should.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace McFly.Server.Data.Test
+{
+    public class NotCriterion_Should
+    {
+        [Fact]
+        public void Invert_The_Query_Result_Set()
+        {
+
+        }
+    }
+}
