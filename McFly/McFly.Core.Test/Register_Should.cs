f35241218398bddd0e425b520d31e93593395b14
Ty Smith <dts5004@gmail.com>
2018-04-07 23:58:33 -0700

    Added Register_Should test class

---
+++ McFly/McFly.Core.Test/Register_Should.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using McFly.Core.Registers;
+using Xunit;
+
+namespace McFly.Core.Test
+{
+    public class Register_Should
+    {
+        [Fact]
+        public void Not_Have_Duplicates()
+        {
+            Register.AllRegisters.Should().OnlyHaveUniqueItems();
+        }
+    }
+}
