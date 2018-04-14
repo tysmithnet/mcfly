45923c01e4c1da6835c1577ea72db6b770ab67e1
Ty Smith <dts5004@gmail.com>
2018-04-13 18:41:04 -0700

    Started writing register regex tests

---
+++ McFly/McFly/RegisterRegexFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using McFly.Core.Registers;
+
+namespace McFly
+{
+    [Export]
+    public class RegisterRegexFactory
+    {
+        public Regex Get(Register register)
+        {
+            if (register == Register.Af)
+            {
+                return new Regex(@"(^|[ ,])af=[a-f0-9]{1}", RegexOptions.IgnoreCase);
+            }
+            if (register == Register.Ah)
+            {
+                return new Regex(@"(^|[ ,])ah=[a-f0-9]{2}");
+            }
+            
+            
+            throw new ArgumentOutOfRangeException($"{nameof(register)} was not found in the factory, this is a programming error.");
+        }
+    }
+}
