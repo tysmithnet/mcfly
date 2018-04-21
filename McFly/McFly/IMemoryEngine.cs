0e5787b95e4220d800b860bc95aa30ef9d825a96
Ty Smith <dts5004@gmail.com>
2018-04-21 01:26:54 -0700

    Extracted IMemoryEngine

---
+++ McFly/McFly/IMemoryEngine.cs
@@ -0,0 +1,9 @@
+﻿using McFly.Debugger;
+
+namespace McFly
+{
+    public interface IMemoryEngine
+    {
+        byte[] ReadMemory(ulong start, ulong end, IDebugDataSpaces dataSpaces, bool is32Bit);
+    }
+}
--- McFly/McFly/MemoryEngine.cs
+++ McFly/McFly/MemoryEngine.cs
@@ -1,30 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using McFly.Core;
 using McFly.Debugger;
 
 namespace McFly
 {
-
-    public class MemoryEngine
+    [Export(typeof(IMemoryEngine))]
+    public class MemoryEngine : IMemoryEngine
     {
         public byte[] ReadMemory(ulong start, ulong end, IDebugDataSpaces dataSpaces, bool is32Bit)
         {
             var length = start < end ? end - start : start - end;
             var buffer = new byte[length];
             if (is32Bit)
-            {
                 start = start < end ? start : end;
-                
-            }
             else
-            {
                 start = start < end ? end : start;
-            }
-            int hr = dataSpaces.ReadVirtual(start, buffer, buffer.Length.ToUInt(), out var bytesRead);
+            var hr = dataSpaces.ReadVirtual(start, buffer, buffer.Length.ToUInt(), out var bytesRead);
             if (hr != 0)
                 throw new ApplicationException($"Unable to read virtual memory at {start:X16}, error code: {hr}");
 
