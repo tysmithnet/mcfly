2ef72df4bf958f2d56047c7d2b5a1e0643491fe6
Ty Smith <dts5004@gmail.com>
2018-04-22 20:26:58 -0700

    Added processing of AddMemoryRequest in MemoryController

--- McFly/McFly.Server/Controllers/MemoryController.cs
+++ McFly/McFly.Server/Controllers/MemoryController.cs
@@ -1,28 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// ***********************************************************************
+// Assembly         : McFly.Server
+// Author           : @tysmithnet
+// Created          : 04-21-2018
+//
+// Last Modified By : @tysmithnet
+// Last Modified On : 04-22-2018
+// ***********************************************************************
+// <copyright file="MemoryController.cs" company="">
+//     Copyright ©  2018
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
 using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Http;
 using Common.Logging;
 using McFly.Server.Contract;
+using McFly.Server.Data;
 using McFly.Server.Headers;
 
 namespace McFly.Server.Controllers
 {
+    /// <summary>
+    ///     Class MemoryController.
+    /// </summary>
+    /// <seealso cref="System.Web.Http.ApiController" />
     [Route("api/memory/")]
     [Export]
     [PartCreationPolicy(CreationPolicy.NonShared)]
     public class MemoryController : ApiController
     {
+        /// <summary>
+        ///     The log
+        /// </summary>
         private readonly ILog Log = LogManager.GetLogger<MemoryController>();
 
-        
-
+        /// <summary>
+        ///     Adds raw memory to the analysis
+        /// </summary>
+        /// <param name="projectName">Name of the project.</param>
+        /// <param name="request">The request.</param>
+        /// <returns>IHttpActionResult.</returns>
         public IHttpActionResult Post([FromProjectNameHeader] string projectName, [FromBody] AddMemoryRequeset request)
         {
+            MemoryAccess.AddMemory(projectName, request.MemoryChunk);
             return Ok();
         }
+
+        /// <summary>
+        ///     Gets or sets the memory access.
+        /// </summary>
+        /// <value>The memory access.</value>
+        [Import]
+        protected internal IMemoryAccess MemoryAccess { get; set; }
     }
 }
