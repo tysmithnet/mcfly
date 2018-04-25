5416245255aafa9a132112205dd12b43f9a706b1
Ty Smith <dts5004@gmail.com>
2018-04-24 20:13:43 -0700

    Consolidated nuget packages and added testing libraries to new project

--- McFly/McFly.Core.JsonConverters.Test/MemoryRangeJsonConverter_Should.cs
+++ McFly/McFly.Core.JsonConverters.Test/MemoryRangeJsonConverter_Should.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace McFly.Core.JsonConverters.Test
 {
     public class MemoryRangeJsonConverter_Should
     {
-        public void 
+        [Fact]
+        public void Write_Correct_Json()
+        {
+
+        }
     }
 }
