d383bac611c99f60dadc700ed2b2d1ace4cc53f9
Ty Smith <dts5004@gmail.com>
2018-04-24 20:07:52 -0700

    Added McFly.Core.JsonConverters.Test project

---
+++ McFly/McFly.Core.JsonConverters.Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McFly.Core.JsonConverters.Test
+{
+    public class MemoryRangeJsonConverter_Should
+    {
+    }
+}
