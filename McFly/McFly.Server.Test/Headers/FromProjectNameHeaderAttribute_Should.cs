dcc808ab4054bc0e0d6b0a6264097a5a1a9ae840
Ty Smith <dts5004@gmail.com>
2018-04-27 22:11:19 -0700

    Added more tests

---
+++ McFly/McFly.Server.Test/Headers/FromProjectNameHeaderAttribute_Should.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http.Controllers;
+using FluentAssertions;
+using McFly.Server.Headers;
+using Xunit;
+
+namespace McFly.Server.Test.Headers
+{
+    public class FromProjectNameHeaderAttribute_Should
+    {
+        [Fact]
+        public void Should_Have_Correct_Name()
+        {
+            var header = new FromProjectNameHeaderAttribute();
+            header.HeaderName.Should().Be("X-Project-Name");
+        }
+
+        [Fact]
+        public void Should_Get_Correct_Binding()
+        {
+            var header = new FromProjectNameHeaderAttribute();
+            header.GetBinding(new ReflectedHttpParameterDescriptor()).Should().BeOfType<FromHeaderBinding>();
+        }
+    }
+}
