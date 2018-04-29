da3b2f5eb10e3ba35d06c28a94e24cbd40605704
Ty Smith <dts5004@gmail.com>
2018-04-29 01:21:35 -0700

    Added FrameDomainEntityConverter

--- McFly/McFly.Server.Data.SqlServer/FrameDomainEntityConverter.cs
+++ McFly/McFly.Server.Data.SqlServer/FrameDomainEntityConverter.cs
@@ -6,36 +6,45 @@
 // Last Modified By : @tysmithnet
 // Last Modified On : 04-29-2018
 // ***********************************************************************
-// <copyright file="IDomainEntityConverter.cs" company="">
+// <copyright file="FrameDomainEntityConverter.cs" company="">
 //     Copyright ©  2018
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
 
+using McFly.Core;
+
 namespace McFly.Server.Data.SqlServer
 {
     /// <summary>
-    ///     Represents an object that is capable of converting between domain objects and
-    ///     database entities
+    ///     Domain entity converter for frames and frameenities
     /// </summary>
-    /// <typeparam name="TDomain">The type of the domain object.</typeparam>
-    /// <typeparam name="TEntity">The type of the db entity.</typeparam>
-    internal interface IDomainEntityConverter<TDomain, TEntity>
+    /// <seealso
+    ///     cref="McFly.Server.Data.SqlServer.IDomainEntityConverter{McFly.Core.Frame, McFly.Server.Data.SqlServer.FrameEntity}" />
+    internal class FrameDomainEntityConverter : IDomainEntityConverter<Frame, FrameEntity>
     {
         /// <summary>
-        ///     Convert an entity to a domain object.
+        ///     Convert a frame entity to a frame domain object.
         /// </summary>
         /// <param name="entity">The entity.</param>
         /// <param name="context">The context.</param>
         /// <returns>The corresponding domain object for the provided entity</returns>
-        TDomain ToDomain(TEntity entity, IMcFlyContext context);
+        /// <inheritdoc />
+        public Frame ToDomain(FrameEntity entity, IMcFlyContext context)
+        {
+            return null;
+        }
 
         /// <summary>
-        ///     Convert a domain object to an entity
+        ///     Convert a frame domain object to an entity
         /// </summary>
         /// <param name="domainObject">The domain object.</param>
         /// <param name="context">The context.</param>
         /// <returns>The corresponding database entity for the provided domain object</returns>
-        TEntity ToEntity(TDomain domainObject, IMcFlyContext context);
+        /// <inheritdoc />
+        public FrameEntity ToEntity(Frame domainObject, IMcFlyContext context)
+        {
+            return null;
+        }
     }
 }
