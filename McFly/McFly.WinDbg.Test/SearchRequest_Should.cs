45b0b5a17cca80dcbe4a0b697c93a2b69fe2eb61
Ty Smith <dts5004@gmail.com>
2018-05-08 19:33:36 -0700

    Added SearchRequest tests

---
+++ McFly/McFly.WinDbg.Test/SearchRequest_Should.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using McFly.WinDbg.Search;
+using Xunit;
+
+namespace McFly.WinDbg.Test
+{
+    public class SearchRequest_Should
+    {
+        [Fact]
+        public void Allow_Search_Filters_To_Be_Added()
+        {
+            var request = new SearchRequest("name", new List<SearchFilter>());
+            request.AddSearchFilter(new SearchFilter());
+        }
+
+        [Fact]
+        public void Require_An_Index()
+        {
+            Action a = () => new SearchRequest(null, new List<SearchFilter>());
+            Action a2 = () => new SearchRequest("name", null);
+
+            a.Should().Throw<ArgumentNullException>();
+            a2.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
