1e02897bc3309c05a4a92f92bb07781d09a0b861
Ty Smith <dts5004@gmail.com>
2018-05-23 01:11:18 -0700

    npm init

--- McFly/mcfly-ui/vagrant/bootstrap.sh
+++ McFly/mcfly-ui/vagrant/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y apache2 curl git
+apt-get update && install -y apache2 curl git
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
+npm i -g yarn
 
 if ! [ -L /var/www ]; then
   rm -rf /var/www
