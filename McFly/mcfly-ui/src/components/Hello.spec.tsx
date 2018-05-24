d027e2e84b2df3ac2fedce7390b7dec2df1d2489
Tyler Smith <tsmith@csod.com>
2018-05-24 13:23:01 -0700

    Fixed broken tests

--- McFly/mcfly-ui/src/components/Hello.spec.tsx
+++ McFly/mcfly-ui/src/components/Hello.spec.tsx
@@ -3,11 +3,6 @@ import { shallow } from "enzyme";
 
 import {Hello} from "./Hello";
 
-import initStoryshots from '@storybook/addon-storyshots';
-initStoryshots({
-    
-});
-
 it("renders the heading", () => {
     const result = shallow(<Hello name="world"/>).contains("world");
     expect(result).toBeTruthy();
