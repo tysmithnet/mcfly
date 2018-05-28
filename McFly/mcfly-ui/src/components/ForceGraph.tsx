ac159b03b7ad5ca4fe6b053db3d7221900f442e1
Ty Smith <dts5004@gmail.com>
2018-05-28 01:26:03 -0700

    Added start of simulator for D3

--- McFly/mcfly-ui/src/components/ForceGraph.tsx
+++ McFly/mcfly-ui/src/components/ForceGraph.tsx
@@ -1,16 +1,38 @@
 import * as React from "react";
 import ForceGraphNode from "../components/ForceGraphNode";
+import ForceGraphLink from "./ForceGraphLink";
+
+const workerPath = require("file-loader?name=[name].ts!./simulator");
 
 interface Props {
   width: number;
   height: number;
 }
 
-interface State {}
+interface State {
+  nodes?: ForceGraphNode[],
+  links?: ForceGraphLink[];
+}
 
 export default class ForceGraph extends React.PureComponent<Props, State> {
+  private webWorker: Worker;
+
   constructor(props: Props, state: State) {
     super(props, state);
+  }
+
+  public componentWillMount(): void {
+    this.webWorker = new Worker(workerPath);
+    this.webWorker.postMessage({
+      links: this.state.links,      
+      nodes: this.state.nodes
+    });
+
+    // todo: this should be typed
+    this.webWorker.onmessage = (event:any):void => { 
+      // tslint:disable-next-line:no-console
+      console.log(event);
+    };
   }
 
   public render(): React.ReactNode {
