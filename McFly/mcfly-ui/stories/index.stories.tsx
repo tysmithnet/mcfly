ca139ec00a1784c9917bd6db630eeff8c139734e
Ty Smith <dts5004@gmail.com>
2018-05-28 00:07:14 -0700

    Fixed broken storybook

--- McFly/mcfly-ui/stories/index.stories.tsx
+++ McFly/mcfly-ui/stories/index.stories.tsx
@@ -1,13 +1,25 @@
-import * as React from 'react';
-
-import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { storiesOf, Story, StoryDecorator } from '@storybook/react';
+import { createBrowserHistory } from "history";
+import * as React from 'react';
+import {Provider} from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
 import ForceGraph from "../src/components/ForceGraph";
-import ForceGraphNode from "../src/components/ForceGraphNode";
 import ForceGraphLink from "../src/components/ForceGraphLink";
+import ForceGraphNode from "../src/components/ForceGraphNode";
+import {configureStore, RootState} from "../src/renameme";
+
+const history = createBrowserHistory({});
+const store = configureStore(history);
+
+const decorator :StoryDecorator = getStory => (
+    <Provider store={store}>
+        {getStory()}
+    </Provider>);
 
 storiesOf('ForceGraph', module)
+    .addDecorator(decorator)
     .add('Empty', () => <ForceGraph width={100} height={100}/>)
     .add("Single Node", () => 
         <ForceGraph width={100} height={100}>
