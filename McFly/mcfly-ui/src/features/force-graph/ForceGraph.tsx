0bd7e25c564d0a4c0525f19cee607569377efde1
Ty Smith <dts5004@gmail.com>
2018-06-03 01:54:31 -0700

    First 3d scene rendered with trackball control

--- McFly/mcfly-ui/src/features/force-graph/ForceGraph.tsx
+++ McFly/mcfly-ui/src/features/force-graph/ForceGraph.tsx
@@ -12,19 +12,30 @@ import {
   AmbientLight,
   BoxGeometry,
   Camera,
+  GridHelper,
+  LightShadow,
   Mesh,
   MeshBasicMaterial,
   MeshPhongMaterial,
+  OrbitControls,
   PerspectiveCamera,
+  PlaneBufferGeometry,
   Renderer,
   Scene,
+  ShadowMaterial,
   Sphere,
   SphereGeometry,
-  TrackballControls,
+  SpotLight,
+  SpotLightShadow,
+  TrackballControls as TrackballControlsType,
+  VRControls,
   WebGLRenderer
 } from "three";
+
 import { ForceGraphElement, ForceGraphLink, ForceGraphNode } from "./domain";
 import "./styles.scss";
+
+const TrackballControls:any = require("three-trackballcontrols");
 
 export interface Props {
   id: string;
@@ -38,6 +49,7 @@ export interface State {
 }
 
 export default class ForceGraph extends React.PureComponent<Props, State> {
+  private spotlight: SpotLight;
   private webWorker: Worker;
   private containerDiv: HTMLDivElement;
   private simulation: Simulation<
@@ -48,7 +60,7 @@ export default class ForceGraph extends React.PureComponent<Props, State> {
   private camera: PerspectiveCamera;
   private renderer: WebGLRenderer;
   private spheres: { [id: string]: Mesh };
-  private trackballControls: TrackballControls;
+  private trackballControls: TrackballControlsType;
   constructor(props: Props, state: State) {
     super(props, state);
     const ref = React.createRef();
@@ -101,28 +113,68 @@ export default class ForceGraph extends React.PureComponent<Props, State> {
   public componentDidMount(): void {
     this.scene = new Scene();
     this.camera = new PerspectiveCamera(
-      45,
+      70,
       this.props.width / this.props.height,
-      0.1,
+      1,
       10000
     );
-    this.camera.updateProjectionMatrix();
-    this.camera.position.z = -300;
+    this.camera.position.set(0, 250, 1000);
     this.renderer = new WebGLRenderer({
       antialias: true
     });
-    this.renderer.setClearColor("#000000");
+
+    this.trackballControls = new TrackballControls(this.camera);
+    this.trackballControls.rotateSpeed = 1.0;
+    this.trackballControls.zoomSpeed = 1.2;
+    this.trackballControls.panSpeed = .8;
+    this.trackballControls.noZoom = false;
+    this.trackballControls.noPan = false;
+    this.trackballControls.noRotate = false;
+    this.trackballControls.staticMoving = true;
+    this.trackballControls.dynamicDampingFactor = .3;
+    this.trackballControls.keys = [65, 83, 68];
+    this.trackballControls.addEventListener('change', this.renderFrame);
+
+    this.renderer.setClearColor(0xf0f0f0);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.props.width, this.props.height);
+    this.renderer.shadowMap.enabled = true;
     this.containerDiv.appendChild(this.renderer.domElement);
     const geometry = new SphereGeometry(10);
     const material = new MeshPhongMaterial({ color: "#433F81" });
     this.scene.add(new AmbientLight(0xbbbbbb));
+
+    const light = new SpotLight(0xffffff, 1.5);
+    light.position.set(0, 1500, 200);
+    light.castShadow = true;
+    light.shadow = new SpotLightShadow(new PerspectiveCamera(70, 1, 200, 2000));
+    light.shadow.bias = -0.000222;
+    light.shadow.mapSize.width = 1024;
+    light.shadow.mapSize.height = 1024;
+    this.scene.add(light);
+    this.spotlight = light;
+
+    const planeGeometry = new PlaneBufferGeometry(2000, 2000);
+    planeGeometry.rotateX(-Math.PI / 2);
+    const planeMaterial = new ShadowMaterial({ opacity: 0.2 });
+    const plane = new Mesh(planeGeometry, planeMaterial);
+    plane.position.y = -200;
+    plane.receiveShadow = true;
+    this.scene.add(plane);
+    const helper = new GridHelper(2000, 100);
+    helper.position.y = -199;
+    helper.material.opacity = 0.25;
+    helper.material.transparent = true;
+    this.scene.add(helper);
+
+
     this.spheres = {};
     this.state.nodes.forEach((e, i) => {
       this.spheres[e.id] = new Mesh(geometry, material);
       this.scene.add(this.spheres[e.id]);
     });
     (window as any).scene = this.scene;
+    this.animate();    
     this.renderFrame();
   }
 
@@ -130,11 +182,16 @@ export default class ForceGraph extends React.PureComponent<Props, State> {
     return <div ref={node => (this.containerDiv = node)} />;
   }
 
-  private renderFrame = () => {
-    requestAnimationFrame(this.renderFrame);
+  private animate = () => {
+    requestAnimationFrame(this.animate);
     this.simulation.nodes().forEach((e, i) => {
-      this.spheres[e.id].position.set(e.x, e.y, e.z);
+      this.spheres[e.id].position.set(e.x || 0, e.y || 0, e.z || 0);
     });
+    this.trackballControls.update();  
+    this.renderFrame();  
+  }
+
+  private renderFrame = () => {
     this.renderer.render(this.scene, this.camera);
   };
 }
