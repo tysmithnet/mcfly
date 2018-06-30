3fdca024e2f5c18363abc2b5091ae2a92b90001d
Ty Smith <dts5004@gmail.com>
2018-06-30 16:25:33 -0700

    Added start of simulation rewrite

---
+++ McFly/mcfly-ui/src/features/force-graph/simulation/index.ts
@@ -0,0 +1,12 @@
+// Well Separated Pair Decomposition Force Directed Graph Implementation
+// http://www.mdpi.com/1999-4893/9/3/53
+
+export default class Simulation
+{
+    private nodes: Set<string>;
+
+    public tick():void {
+        ;
+    }
+}
+
