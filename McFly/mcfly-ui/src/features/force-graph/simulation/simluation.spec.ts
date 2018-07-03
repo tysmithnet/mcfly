36667be94eb818ef14ca18abbae39577d1c0bc0b
Ty Smith <dts5004@gmail.com>
2018-07-03 00:58:36 -0700

    Organized simulation tests

--- McFly/mcfly-ui/src/features/force-graph/simulation/simluation.spec.ts
+++ McFly/mcfly-ui/src/features/force-graph/simulation/simluation.spec.ts
@@ -1,46 +1,60 @@
 import { SimulationEngine } from ".";
 
-it("should handle an empty array", () => {
-    const masses = new Float32Array(0);
-    const positions = new Float32Array(0);
-    const links = new Float32Array(0);
-
-    const simulation = new SimulationEngine(masses, positions, links, 1);
-    simulation.tick();
-    const result = simulation.getPositions();
-    expect(result).toEqual(new Float32Array(0));
-});
-  
-it("should reject poorly sized arrays", () => {
-    const masses = new Float32Array(0);
-    const positions = new Float32Array(1);
-    const links = new Float32Array(0);
-
-    const throws = () => new SimulationEngine(masses, positions, links, 1);
-    expect(throws).toThrow();
+describe("trivial", () => {
+    it("should handle empty arrays", () => {
+        const masses = new Float32Array(0);
+        const positions = new Float32Array(0);
+        const links = new Float32Array(0);
+    
+        const simulation = new SimulationEngine(masses, positions, links, 1);
+        simulation.tick();
+        const result = simulation.getPositions();
+        expect(result).toEqual(new Float32Array(0));
+    });
 });
 
-it("should reject numDimensions not in (1,2,3)", () => {
-    const masses = new Float32Array(0);
-    const positions = new Float32Array(0);
-    const links = new Float32Array(0);
-
-    const throws0 = () => new SimulationEngine(masses, positions, links, 0);
-    const throws1 = () => new SimulationEngine(masses, positions, links, 1.2);
-    const throws2 = () => new SimulationEngine(masses, positions, links, -1);
-    const throws3 = () => new SimulationEngine(masses, positions, links, 4);
-    expect(throws0).toThrow();
-    expect(throws1).toThrow();
-    expect(throws2).toThrow();
-    expect(throws3).toThrow();
+describe("error handling", () => {
+    it("should reject poorly sized arrays", () => {
+        const masses = new Float32Array(0);
+        const positions = new Float32Array(1);
+        const links = new Float32Array(0);
+        const throws = () => new SimulationEngine(masses, positions, links, 1);
+        expect(throws).toThrow();
+    });
+    
+    it("should reject numDimensions not in (1,2,3)", () => {
+        const masses = new Float32Array(0);
+        const positions = new Float32Array(0);
+        const links = new Float32Array(0);
+        const throws0 = () => new SimulationEngine(masses, positions, links, 0);
+        const throws1 = () => new SimulationEngine(masses, positions, links, 1.2);
+        const throws2 = () => new SimulationEngine(masses, positions, links, -1);
+        const throws3 = () => new SimulationEngine(masses, positions, links, 4);
+        expect(throws0).toThrow();
+        expect(throws1).toThrow();
+        expect(throws2).toThrow();
+        expect(throws3).toThrow();
+    });
 });
 
-it("pass a basic 1D test with 2 nodes", () => {
-    const masses = Float32Array.from([1,1]);
-    const positions = Float32Array.from([1,5]);
-    const links = new Float32Array(0);
-    const simulation = new SimulationEngine(masses, positions, links, 1);
-    simulation.tick();
-    const result = simulation.getPositions();
-    expect(result).toEqual(Float32Array.from([1.0625, 4.9375]));
+describe("1D", () => {
+    it("should not move a solitary node", () => {
+        const masses = Float32Array.from([1]);
+        const positions = Float32Array.from([1]);
+        const links = new Float32Array(0);
+        const simulation = new SimulationEngine(masses, positions, links, 1);
+        simulation.tick();
+        const result = simulation.getPositions();
+        expect(result).toEqual(Float32Array.from([1]));
+    });
+    
+    it("should act on a pair of nodes", () => {
+        const masses = Float32Array.from([1,1]);
+        const positions = Float32Array.from([1,5]);
+        const links = new Float32Array(0);
+        const simulation = new SimulationEngine(masses, positions, links, 1);
+        simulation.tick();
+        const result = simulation.getPositions();
+        expect(result).toEqual(Float32Array.from([1.0625, 4.9375]));
+    });
 });
